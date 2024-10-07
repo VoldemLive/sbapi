0c3881a4c54f1f7b76f886c7e5dee107789bdf6c
Volodymyr Shynkarov <3dmirok@gmail.com>
2024-10-07 00:15:44 -0700

    list of CORS gets from .env, added .env.example

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'
+    origins ENV['CORS_ALLOWED_ORIGINS'].split(',')
 
     resource "*",
       headers: :any,
